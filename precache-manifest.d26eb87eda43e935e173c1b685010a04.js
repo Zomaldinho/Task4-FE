5fe074b8872bf18ecfb9dfee89e4ee18925e9960
Zoma <hazem_ahmed_93@yahoo.com>
2020-06-28 11:22:11 +0200

    Updates

---
+++ precache-manifest.d26eb87eda43e935e173c1b685010a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947e8c2de2a54ab4442f00619e29a61a",
+    "url": "/Task4-FE/index.html"
+  },
+  {
+    "revision": "508ee467777acd6c36d3",
+    "url": "/Task4-FE/static/css/main.9c973d81.chunk.css"
+  },
+  {
+    "revision": "75010dc584a2d4ccbb9b",
+    "url": "/Task4-FE/static/js/2.554af844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task4-FE/static/js/2.554af844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508ee467777acd6c36d3",
+    "url": "/Task4-FE/static/js/main.a6dc7e63.chunk.js"
+  },
+  {
+    "revision": "5afd9879550b8956d36e",
+    "url": "/Task4-FE/static/js/runtime-main.34bae8dd.js"
+  }
+]);
